858e274386f535d6d5be535e3e2fb27b7fd28bb3
AndreyKo-ko <145385233+AndreyKo-ko@users.noreply.github.com>
2023-10-20 21:15:13 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = " https://8d417e39-9862-4e00-baf9-67c2eb0cbcba.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+CREATE_KITS_PATH = "/api/v1/kits/"
